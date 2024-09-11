8c8524974022b451a74b6c78b4ea434e5f27dd2f
annie <annierose12231@gmail.com>
2024-09-10 20:33:13 -0400

    init
    init setup

---
+++ tmpl.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(require "racket/lib/common.rkt"
+         rackunit)
+
+;; pt 1                    
+(define dayNa
+  (λ (ls)
+    (void)))
+
+;; pt 2
+(define dayNb
+  (λ (ls)
+    (void)))
+
+;; main
+(module+ main
+  (call-with-input-file "data/dayN.txt"
+    (lambda (prt)
+      (define lines (port->lines prt))
+      (answer N 1 (dayNa lines))
+      (answer N 2 (dayNb lines)))))
